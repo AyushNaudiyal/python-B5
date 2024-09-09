f365f2752250e94739542046d0995f7cf0abf9ee
AyushNaudiyal <Ayush.17655@stu.upes.ac.in>
2024-09-09 11:15:35 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,14 @@
+++PI = 3.14
+
+def add(a, b):
+    return a + b
+
+def diff(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print(f"print addition of (a) +++and (b) = {add(a, b)}")
+else:
+    print("imported as module")
